4c9935af21e287ad6aea9038130d6c28c4dc62cf
Jules ten Bos <jules.tenbos@gmail.com>
2024-05-23 13:24:42 +0100

    done

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# local-drive-demo
-The local drive example revisited.
+# localdrive-demo
+
+Leg 1 of the journey, the localdrive example revisited.
+The original example is here: https://docs.pears.com/helpers/localdrive.
+
+At the core of the __Pear__ platform is the brilliance of how it deals with data across the swarm, and on the local filesystem.  
+__Pear__ implements __Hyperdrive__, a secure real-time distributed file system designed for easy P2P file sharing.  
+It features a simple API that instead of mirroring POSIX APIs, captures better the core requirements of P2P file transfer.
+
+The __localdrive__ module is an auxiliary tool of the __Hyperdrive__ family .  
+It makes it easy to work with files on the local filesystem that streamline import/export flows and make it easy to mirror drives to and from the local filesystem.
+
+This example illustrates the basic operations of __localdrive__.  
+See the code in _index.js_ for details.
+
+To run a Pear app from code, execute the command below, assuming the working folder is set to the project folder.
+```bash
+pear run dev .
+```
--- index.js
+++ index.js
@@ -1,39 +1,62 @@
 import Localdrive from 'localdrive' 
-//const Localdrive = require('localdrive')
 
-const drive = new Localdrive('./my-project')
+const drive = new Localdrive('./my-localdrive')
 
+console.log('Deleting existing files within ./my-localdrive ')
+for await (const file of drive.list('/')) {
+ console.log('Deleting ', file.key)
+ await drive.del(file.key)
+}
+
+// Write a file all at once
+console.log('Creating text file /blob.txt')
 await drive.put('/blob.txt', Buffer.from('example'))
+
+console.log('Creating executable /test.exe')
+await drive.put('/test.exe', Buffer.from('   '),{ executable: true })
+
+console.log('Creating some image files')
 await drive.put('/images/logo.png', Buffer.from('..'))
 await drive.put('/images/old-logo.png', Buffer.from('..'))
 
+// Read a file all at once
 const buffer = await drive.get('/blob.txt')
-console.log(buffer) // => <Buffer ..> 'example'
+console.log('The binary contents of /blob.txt', buffer)
 
+// list a file entry
+console.log('The file entry details of /blob.txt')
 const entry = await drive.entry('/blob.txt')
-console.log(entry) // => { key, value: { executable, linkname, blob, metadata } }
+console.log(entry) 
 
+// delete a file
+console.log('Deleting /images/old-logo.png')
 await drive.del('/images/old-logo.png')
 
+// create a symlink
+console.log('Creating shortcut /images/logo.shortcut')
 await drive.symlink('/images/logo.shortcut', '/images/logo.png')
 
-
+// list files in a folder
+console.log('Listing the file contents within ./my-localdrive/images')
 for await (const file of drive.list('/images')) {
     console.log('list', file) // => { key, value }
-  }
+ }
 
-  for await (const file of drive.list('/')) {
-    console.log('list', file) // => { key, value }
-  }
-    
+ console.log('Listing the file contents within ./my-localdrive ')
+ for await (const file of drive.list('/')) {
+  console.log('list', file) // => { key, value }
+}
 
+// Read a file in chunks
+console.log('Reading /blob.txt in chunks')
 const rs = drive.createReadStream('/blob.txt')
 for await (const chunk of rs) {
   console.log('rs', chunk) // => <Buffer ..>
 }
 
+// Write a file in chunks
+console.log('Rewriting /blob.txt in chunks')
 const ws = drive.createWriteStream('/blob.txt')
 ws.write('new example')
 ws.end()
 ws.once('close', () => console.log('file saved'))
-
